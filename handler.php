c287b29106b0daad4c726349b662508a18e597ee
Wyatt Ferguson <116971051+wyattferg@users.noreply.github.com>
2022-12-23 04:06:43 -0600

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,11 +1,3 @@
-<?
-$mail_to_send_to = "contact@thewyattjames.com";
-$from_email = "wjfgiggem92@gmail.com";   
-$name=$_REQUEST['name'];
-$email=$_REQUEST['email'];
-$message= "\r\n" . "Name: $name" . "\r\n"; //get recipient name in contact form
-$message = $message.$_REQUEST['message'] . "\r\n" . "from: " . $email; //add message and email from the contact form to existing message(name of the client)
-
 $mail = new PHPMailer(); // create a new object
 $mail->IsSMTP(); // enable SMTP
 $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
@@ -14,16 +6,15 @@ $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
 $mail->Host = "smtp.gmail.com";
 $mail->Port = 465; // or 587
 $mail->IsHTML(true);
-$mail->Username = $from_email;
+$mail->Username = "wjfgiggem92@gmail.com";
 $mail->Password = "recycle100/100";
-$mail->SetFrom($from_email);
+$mail->SetFrom("wjfgiggem92@gmail.com");
 $mail->Subject = "Test";
-$mail->Body = $message;
-$mail->AddAddress($mail_to_send_to);
+$mail->Body = "hello";
+$mail->AddAddress("wyattferguson@yahoo.com");
 
  if(!$mail->Send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
  } else {
     echo "Message has been sent";
  }
-        ?>
