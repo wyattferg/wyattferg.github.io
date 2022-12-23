7f16c3a202485ef13ade6dbdfe13b67acd79fd4e
Wyatt Ferguson <116971051+wyattferg@users.noreply.github.com>
2022-12-23 03:19:16 -0600

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,6 +1,6 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "email@example.com"; // this is your Email address
+    $to = "contact@thewyattjames.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $full_name = $_POST['full_name'];
     $subject = "Form submission";
