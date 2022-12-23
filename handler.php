e5d263ca5fd6afa954543dd8aa60d7c4f6215cb2
Wyatt Ferguson <116971051+wyattferg@users.noreply.github.com>
2022-12-23 03:29:09 -0600

    Update handler.php

--- handler.php
+++ handler.php
@@ -6,11 +6,8 @@ if(isset($_POST['submit'])){
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
     $message = $full_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $full_name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     }
 ?>
